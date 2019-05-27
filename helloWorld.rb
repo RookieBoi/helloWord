a32a42891936fef2e9119cb23dc800c7e2f8e94a
boyuan yao <byao0303@gmail.com>
2019-05-27 11:41:10 -0500

    Modified helloWorld.rb

--- helloWorld.rb
+++ helloWorld.rb
@@ -2,3 +2,11 @@ puts "hi, first program. Hello world"
 puts "Please enter your name:"
 i = gets.chomp
 puts "Hello, " + i + " how do you do"
+puts "Please enter your favorite movie:"
+movie = gets.chomp
+puts (movie + " is an awesome movie")
+j = 0
+for i in [1,2,3,4]
+  j = j+1
+  puts (j).to_s
+end
