a3a9ceddcf08b8d4c2792dc8c5989067fa2b2e3c
boyuan yao <byao0303@gmail.com>
2019-05-27 11:31:53 -0500

    Added get user input

--- helloWorld.rb
+++ helloWorld.rb
@@ -1,6 +1,4 @@
 puts "hi, first program. Hello world"
-i = "1"
-puts "I like the number " + i
-for i in [1,2,3,4]
-  puts i
-end
+puts "Please enter your name:"
+i = gets.chomp
+puts "Hello, " + i + " how do you do"
